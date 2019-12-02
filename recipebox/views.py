b22bfa5723892dab75010f0684cb44da2a073396
Mark Harsley <mharsley45@gmail.com>
2019-12-02 15:32:16 -0500

    authcheck

--- recipebox/views.py
+++ recipebox/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, HttpResponseRedirect, reverse
-from recipebox.models import RecipeItem
-from recipebox.models import Author
-from recipebox.forms import AuthorAdd, NewsItemAdd_
 from django.utils import timezone
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
+from recipebox.models import RecipeItem, Author
+from recipebox.forms import AuthorAdd, NewsItemAdd, LoginForm
 
 
 def index(request):
@@ -11,6 +12,7 @@ def index(request):
     return render(request, html, {'data': recipes})
 
 
+@login_required
 def recipe_item_view(request, key_id):
     html = 'item_page.html'
     recipe = RecipeItem.objects.get(pk=key_id)
@@ -33,8 +35,8 @@ def add_author_view(request):
         form = AuthorAdd(request.POST)
         if form.is_valid():
             data = form.cleaned_data
-            Author.objects.create(name=data['name'], bio=data['bio'])
-            return HttpResponseRedirect(reverse('homepage'))
+            Author.objects.create(name=data['name'])
+        return HttpResponseRedirect(reverse('homepage'))
     form = AuthorAdd()
     return render(request, html, {'form': form})
 
@@ -42,7 +44,7 @@ def add_author_view(request):
 def add_item_view(request):
     html = "item_add.html"
     if request.method == 'POST':
-        form = NewsItemAdd_(request.POST)
+        form = NewsItemAdd(request.POST)
         if form.is_valid():
             data = form.cleaned_data
             RecipeItem.objects.create(
@@ -52,7 +54,26 @@ def add_item_view(request):
                 prep_time=data['prep_time'],
                 instructions=data['instructions'],
                 post_date=timezone.now()
-                )
-        return HttpResponseRedirect(reverse('homepage'))
-    form = NewsItemAdd_()
+            )
+            return HttpResponseRedirect(reverse('homepage'))
+    form = NewsItemAdd()
     return render(request, html, {'form': form})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            user = authenticate(
+                username=data['username'],
+                password=data['password']
+            )
+            if user:
+                login(request, user)
+                return HttpResponseRedirect(reverse('homepage'))
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('login'))
